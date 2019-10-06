4792bd8ce5ba0cb007a48bbf44b477e0cb4b8bae
GNet <mah0006@hazelglencollege.com>
2019-10-06 16:17:10 +1100

    files
    all in asset folder and scene in scene folder

---
+++ Clock.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Clock : MonoBehaviour
+{
+    public int seconds;
+    public int minutes;
+    public int hours;
+    public string amPm;
+    public Text clockText;
+    public Image imgSecond, imgMin, imgHour;
+
+    public string currentTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        seconds = DateTime.Now.Second;
+        minutes = DateTime.Now.Minute;
+        hours = DateTime.Now.Hour;
+        amPm = DateTime.Now.ToString("tt");
+
+        InvokeRepeating("UpdateWidget", 0,1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Add code to keep the clock updated here
+
+
+        // Call to Update the Widget
+        
+    }
+
+    private void UpdateWidget()
+    {
+        // Add code to update the text on the widget to display the current time
+        // currentTime = ???
+
+
+        seconds = (seconds + 1) % 60;
+
+        if (seconds == 0)
+        {
+            minutes = (minutes + 1) % 60;
+            if (minutes == 0)
+            {
+                hours = (hours + 1) % 24;
+            }
+        }
+
+        imgSecond.transform.localRotation = Quaternion.Euler(0, 0, -seconds * 6);
+        imgMin.transform.localRotation = Quaternion.Euler(0, 0, -minutes * 6);
+        imgHour.transform.localRotation = Quaternion.Euler(0, 0, -(hours * 30 + minutes / 6.0f));
+        
+        clockText.text = GetNumString(hours) + ":" + GetNumString(minutes) + ":" + GetNumString(seconds) + " " + amPm;
+
+        //
+    }
+    private string GetNumString(int num)
+    {
+        return num < 10 ? "0" + num : num.ToString();
+    }
+}
---
+++ Explosim.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Explosim : MonoBehaviour
+{
+
+    private void Awake()
+    {
+        string server = "localhost";
+        string database = "connectcsharptomysql";
+        string uid = "username";
+        string password = "password";
+        string connectionString;
+
+        connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+        database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+
+    }
+    void Start()
+    {
+        foreach (Transform child in transform)
+        {
+            Rigidbody rb = child.gameObject.AddComponent<Rigidbody>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
